d01f9f68b9ac9dfaa15218b53b492efc6dcb3366
Kelly <kellyhowes83@gmail.com>
2024-05-02 14:15:08 +0100

    Added further tests and changed some const names

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,28 @@
 import { render, screen, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import Students from './components/Students';
 import Header from './components/Header'
+import Footer from './components/Footer'
 import IndividualStudent from './components/IndividualStudent'
-import '@testing-library/jest-dom'
-import { getStudents } from './firebase';
+
+const date = new Date();
+const year = date.getFullYear();
+
+it("Header text and image displays", () => {
+    render(<Header />);
+    const headerMessage = screen.getByText(/student records/i)
+    const headerImg = screen.getByRole('img', { name: 'school logo' })
+    expect(headerMessage).toBeVisible();
+    expect(headerImg).toBeVisible();
+})
+
+it("Footer text displays", () => {
+    render(<Footer />);
+    const footerMessage = screen.getByText(/Copyright Wilson College/i)
+    const footerYear = screen.getByText(year)
+    expect(footerMessage).toBeVisible();
+    expect(footerYear).toBeVisible();
+})
 
 
 it("When page is Loading, loading text displays", () => {
@@ -12,9 +31,4 @@ it("When page is Loading, loading text displays", () => {
     waitFor(() => expect(loadingMessage).toBeVisible());
 })
 
-it("Header text displays", () => {
-    render(<Header />);
-    const headerMessage = screen.getByText(/student records/i)
-    waitFor(() => expect(headerMessage).toBeVisible());
-})
 
