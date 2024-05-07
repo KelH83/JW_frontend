4637bbce415596c2905fa3b215a76d4c6aab8fe8
Kelly <kellyhowes83@gmail.com>
2024-05-07 10:56:18 +0100

    Added more tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,34 +1,70 @@
 import { render, screen, waitFor } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Students from './components/Students';
 import Header from './components/Header'
 import Footer from './components/Footer'
-import IndividualStudent from './components/IndividualStudent'
+import Students from './components/Students';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 
-const date = new Date();
-const year = date.getFullYear();
 
-it("Header text and image displays", () => {
+
+describe("Header component", () => {
+  it("Displays header text and image", () => {
     render(<Header />);
     const headerMessage = screen.getByText(/student records/i)
     const headerImg = screen.getByRole('img', { name: 'school logo' })
     expect(headerMessage).toBeVisible();
     expect(headerImg).toBeVisible();
+  })
 })
 
-it("Footer text displays", () => {
-    render(<Footer />);
-    const footerMessage = screen.getByText(/Copyright Wilson College/i)
-    const footerYear = screen.getByText(year)
-    expect(footerMessage).toBeVisible();
-    expect(footerYear).toBeVisible();
+describe("Footer component", () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  it("Displays footer text", () => {
+      render(<Footer />);
+      const footerMessage = screen.getByText(/copyright wilson college/i)
+      const footerYear = screen.getByText(year)
+      expect(footerMessage).toBeVisible();
+      expect(footerYear).toBeVisible();
+  })
 })
 
 
-it("When page is Loading, loading text displays", () => {
-    render(<Students />);
-    const loadingMessage = screen.getByText(/loading/i)
-    waitFor(() => expect(loadingMessage).toBeVisible());
+describe("Students component", () => {
+  it("Displays loading text when page is still loading", () => {
+      render(<Students />);
+      const loadingMessage = screen.getByText(/loading/i)
+      expect(loadingMessage).toBeVisible();
+  })
+  
+  const MockStudents = () => {
+    return (
+      <BrowserRouter>
+        <Students />
+      </BrowserRouter>
+    )
+  }
+  
+  it("Has an add student button on the students page", async () => {
+      render(<MockStudents />)
+      const addButton = await screen.findByTestId('add-button')
+      expect(addButton).toBeInTheDocument()
+  })
+
+  it("Retrieves a students details", async () => {
+    render(<MockStudents />)
+    const student = await screen.findByTestId('student-0')
+    expect(student).toBeInTheDocument()
 })
+
+  it("has a see more button", async () => {
+   render(<MockStudents />)
+   const seeMoreButton = await screen.findByTestId('see-more-1')
+   expect(seeMoreButton).toBeInTheDocument()
+  })
+})
+
+
+
 
 
